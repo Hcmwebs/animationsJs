677e536d24b2ef74c1082b9c032d3ce052d6aebf
Hezekia Chege <zeekprojectos@gmail.com>
2020-10-19 16:00:47 +1300

    projects files created
    Created all the html, Sass and Js files for the project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+let card =document.querySelector('.card');
+let active = false;
+card.addEventListener('click', () => {
+  if( !active){
+    card.classList.add ('active');
+    active = true;
+
+  }
+  else{
+    card.classList.remove('active');
+    active = false;
+  }
+} )
+
+
+
+
